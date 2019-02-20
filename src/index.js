352f3ac96a01ad98d181220650668f78d94e7f2f
Jon-Millent <hanshaohua@yorkg.com>
2019-02-20 17:17:39 +0800

    透明导航栏

--- src/index.js
+++ src/index.js
@@ -26,17 +26,17 @@ export default App;
 
 import {
   View,
-  Text
+  Text,
 } from 'react-native'
 
 import MainScreen from './views/Main';
 
-import {createStackNavigator, createAppContainer, createBottomTabNavigator} from 'react-navigation';
-const HomeScreen = () => (
-  <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-    <Text>Home Screen</Text>
-  </View>
-);
+import {
+  createStackNavigator,
+  createAppContainer,
+  createBottomTabNavigator,
+} from 'react-navigation';
+
 
 const DetailScreen = () => (
   <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
@@ -44,9 +44,41 @@ const DetailScreen = () => (
   </View>
 );
 
-const TabNavigator = createBottomTabNavigator({
-  Home: MainScreen,
-  Detail: DetailScreen,
+const tabOptions = {
+  Main: {
+    screen: MainScreen
+  },
+  Checkout: {
+    screen: DetailScreen
+  }
+};
+
+const TabbedNavigation = createBottomTabNavigator(tabOptions, {
+  initialRouteName: 'Main',
+  animationEnabled: false,
+  swipeEnabled: false,
+  lazyLoad: true,
+  tabBarOptions: {
+    tinColor: '#fff',
+    activeTintColor: '#eee',
+    inactiveTintColor: '#fff',
+    showIcon: true,
+    showLabel: true,
+    lazyLoad: true,
+    upperCaseLabel: false,
+    indicatorStyle: {
+      backgroundColor: 'transparent'
+    },
+    style: {
+      backgroundColor: 'rgba(22, 22, 22, 0.3)',
+      borderTopWidth: 3,
+      borderTopColor: '#996600',
+      position: 'absolute',
+      left: 0,
+      right: 0,
+      bottom: 0
+    }
+  }
 });
 
-export default createAppContainer(TabNavigator);
+export default createAppContainer(TabbedNavigation);
